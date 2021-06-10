2e68bc12910c224de9f5b1f51915b486fa6f7b1f
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-06-09 23:16:40 -0300

    lint(General): lint all files with 2 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,114 +1,114 @@
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 2020,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    'jest/globals': true
+  },
+  rules: {
+    'array-callback-return': 'error',
+    'default-case': 'error',
+    'eol-last': 'error',
+    'func-call-spacing': ['error', 'never'],
+    'init-declarations': ['error', 'always'],
+    'max-len': ['error', { 'ignoreComments': true }, { 'code': 120 }],
+    'max-lines-per-function': ['error', { 'max': 30 }],
+    'max-lines': ['warn', { 'max': 500 }],
+    'no-alert': 'error',
+    'no-console': 'error',
+    'no-empty-function': 'error',
+    'no-extra-parens': 'error',
+    'no-extra-semi': 'error',
+    'no-new': "error",
+    'no-new-func': "error",
+    'no-trailing-spaces': 'error',
+    'no-unused-vars': "error",
+    'semi-spacing': 'error',
+    complexity: ["error", 6],
+    indent: ['error', 2],
+    semi: 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: [
+        '@typescript-eslint',
+      ],
+      rules: {
+        '@typescript-eslint/array-type': 'error',
+        '@typescript-eslint/consistent-type-assertions': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/member-delimiter-style': 'error',
+        '@typescript-eslint/member-ordering': 'error',
+        '@typescript-eslint/method-signature-style': 'error',
+        '@typescript-eslint/naming-convention': 'error',
+        '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+        '@typescript-eslint/no-empty-interface': 'error',
+        '@typescript-eslint/no-extra-non-null-assertion': 'error',
+        '@typescript-eslint/no-misused-new': 'error',
+        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        '@typescript-eslint/typedef': 'error',
+      }
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
         sourceType: 'module',
+        ecmaVersion: 2020,
+      },
+      plugins: [
+        'vue',
+      ],
+      extends: [
+        'plugin:vue/recommended',
+        'plugin:vue/essential',
+      ],
+      rules: {
+        'vue/html-indent': ['error', 4],
+        'vue/multiline-html-element-content-newline': 'error',
+        'vue/no-dupe-keys': 'error',
+        'vue/no-duplicate-attributes': 'error',
+        'vue/no-parsing-error': 'error',
+        'vue/no-reserved-keys': 'error',
+        'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+        'vue/no-unused-components': 'error',
+        'vue/no-use-v-if-with-v-for': 'error',
+        'vue/require-render-return': 'error',
+        'vue/require-v-for-key': 'error',
+      }
     },
-    env: {
-        browser: true,
-        'jest/globals': true
-    },
-    rules: {
-        'array-callback-return': 'error',
-        'default-case': 'error',
-        'eol-last': 'error',
-        'func-call-spacing': ['error', 'never'],
-        'init-declarations': ['error', 'always'],
-        'max-len': ['error', { 'ignoreComments': true }, { 'code': 120 }],
-        'max-lines-per-function': ['error', { 'max': 30 }],
-        'max-lines': ['warn', { 'max': 500 }],
-        'no-alert': 'error',
-        'no-console': 'error',
-        'no-empty-function': 'error',
-        'no-extra-parens': 'error',
-        'no-extra-semi': 'error',
-        'no-new': "error",
-        'no-new-func': "error",
-        'no-trailing-spaces': 'error',
-        'no-unused-vars': "error",
-        'semi-spacing': 'error',
-        complexity: ["error", 6],
-        indent: ['error', 4],
-        semi: 'error',
-    },
-    overrides: [
-        {
-            files: ['*.ts'],
-            parser: '@typescript-eslint/parser',
-            plugins: [
-                '@typescript-eslint',
-            ],
-            rules: {
-                '@typescript-eslint/array-type': 'error',
-                '@typescript-eslint/consistent-type-assertions': 'error',
-                '@typescript-eslint/explicit-function-return-type': 'error',
-                '@typescript-eslint/explicit-module-boundary-types': 'error',
-                '@typescript-eslint/member-delimiter-style': 'error',
-                '@typescript-eslint/member-ordering': 'error',
-                '@typescript-eslint/method-signature-style': 'error',
-                '@typescript-eslint/naming-convention': 'error',
-                '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-                '@typescript-eslint/no-empty-interface': 'error',
-                '@typescript-eslint/no-extra-non-null-assertion': 'error',
-                '@typescript-eslint/no-misused-new': 'error',
-                '@typescript-eslint/no-require-imports': 'error',
-                '@typescript-eslint/type-annotation-spacing': 'error',
-                '@typescript-eslint/typedef': 'error',
-            }
-        },
-        {
-            files: ['*.vue'],
-            parser: 'vue-eslint-parser',
-            parserOptions: {
-                sourceType: 'module',
-                ecmaVersion: 2020,
-            },
-            plugins: [
-                'vue',
-            ],
-            extends: [
-                'plugin:vue/recommended',
-                'plugin:vue/essential',
-            ],
-            rules: {
-                'vue/html-indent': ['error', 4],
-                'vue/multiline-html-element-content-newline': 'error',
-                'vue/no-dupe-keys': 'error',
-                'vue/no-duplicate-attributes': 'error',
-                'vue/no-parsing-error': 'error',
-                'vue/no-reserved-keys': 'error',
-                'vue/no-spaces-around-equal-signs-in-attribute': 'error',
-                'vue/no-unused-components': 'error',
-                'vue/no-use-v-if-with-v-for': 'error',
-                'vue/require-render-return': 'error',
-                'vue/require-v-for-key': 'error',
-            }
-        },
-        {
-            files: ['*.spec.js'],
-            extends: [
-                'plugin:jest/recommended',
-            ],
-            plugins: [
-                'jest',
-            ],
-            rules: {
-                'max-lines': 'off',
-                'max-lines-per-function': 'off',
-                'jest/consistent-test-it': 'error',
-                'jest/expect-expect': 'error',
-                'jest/no-commented-out-tests': 'error',
-                'jest/no-disabled-tests': 'error',
-                'jest/no-export': 'error',
-                'jest/no-focused-tests': 'error',
-                'jest/no-identical-title': 'error',
-                'jest/no-mocks-import': 'error',
-                'jest/prefer-strict-equal': 'error',
-                'jest/prefer-to-be-null': 'error',
-                'jest/prefer-to-be-undefined': 'error',
-                'jest/prefer-to-contain': 'error',
-                'jest/prefer-to-have-length': 'error',
-                'jest/valid-expect': 'error',
-            }
-        }
-    ]
+    {
+      files: ['*.spec.js'],
+      extends: [
+        'plugin:jest/recommended',
+      ],
+      plugins: [
+        'jest',
+      ],
+      rules: {
+        'max-lines': 'off',
+        'max-lines-per-function': 'off',
+        'jest/consistent-test-it': 'error',
+        'jest/expect-expect': 'error',
+        'jest/no-commented-out-tests': 'error',
+        'jest/no-disabled-tests': 'error',
+        'jest/no-export': 'error',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/no-mocks-import': 'error',
+        'jest/prefer-strict-equal': 'error',
+        'jest/prefer-to-be-null': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-contain': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/valid-expect': 'error',
+      }
+    }
+  ]
 };
--- src/VueHicons.vue
+++ src/VueHicons.vue
@@ -39,227 +39,227 @@ import FilledDoublePathIcons from "./assets/FilledDoublePathIcons";
 import TriplePathFilledIcons from "./assets/TriplePathFilledIcons";
 
 export default {
-    name: 'VueHicons',
+  name: 'VueHicons',
 
-    props: {
-        clipRule: {
-            type: String,
-            default: ''
-        },
-
-        fillRulePath2: {
-            type: String,
-            default: ''
-        },
-
-        clipRulePath2: {
-            type: String,
-            default: ''
-        },
-
-        name: {
-            type: String,
-            required: true
-        },
-
-        isFilled: {
-            type: Boolean,
-            default: false
-        },
-
-        fillColor: {
-            type: String,
-            default: 'none'
-        },
-
-        fillRule: {
-            type: String,
-            default: 'nonzero'
-        },
-
-        classIcon: {
-            type: String,
-            default: ''
-        },
-
-        strokeColor: {
-            type: String,
-            default: 'currentColor'
-        },
-
-        strokeWidth: {
-            type: Number,
-            default: 2
-        },
-
-        viewBox: {
-            type: String,
-            default: '0 0 24 24'
-        },
-
-        strokeLinecap: {
-            type: String,
-            default: 'round'
-        },
-
-        strokeLinejoin: {
-            type: String,
-            default: 'round'
-        },
-
-        size: {
-            type: [String, Number],
-            default: '4'
-        }
+  props: {
+    clipRule: {
+      type: String,
+      default: ''
     },
 
-    data: () => ({
-        doublePath: false,
-        isTriplePath: false,
-        icon: {
-            'path1': '',
-            'path2': ''
-        },
-        iconsWithDoublePath: DoublePathIcons,
-        filledIconsWithDoublePath: FilledDoublePathIcons,
-        filledIconsWithTriplePaths: TriplePathFilledIcons,
-    }),
-
-    computed: {
-        iconNameClass() {
-            return this.name.split('_').join('-');
-        },
-
-        classIconFinal () {
-            return `${this.iconNameClass} ${this.classIcon} w-${this.size} h-${this.size}`;
-        },
-
-        clipRuleData() {
-            return this.name == 'volume_off' ? 'evenodd' : this.clipRule;
-        },
-
-        icons() {
-            return this.isFilled
-                ? icons.filled
-                : icons;
-        },
-
-        strokeStyleComponent() {
-            return this.isFilled && this.strokeWidth === 2
-                ? 0
-                : this.strokeWidth;
-        },
-
-        fillStyleComponent() {
-            return this.isFilled && this.fillColor === 'none'
-                ? 'currentColor'
-                : this.fillColor;
-        },
-
-        viewBoxComponent() {
-            return this.isFilled && this.viewBox === '0 0 24 24'
-                ? '0 0 20 20'
-                : this.viewBox;
-        },
-
-        fillRuleBasicPath() {
-            return this.isFilled && this.fillRule === 'nonzero'
-                ? 'evenodd'
-                : this.fillRule;
-        },
-
-        clipRuleBasicPath() {
-            return this.isFilled && this.clipRuleData === 'nonzero'
-                ? 'evenodd'
-                : this.clipRuleData;
-        },
-
-        fillRuleTwoPath() {
-            return this.isFilled && this.fillRulePath2 === '' && this.isDoublePath
-                ? 'evenodd'
-                : this.fillRulePath2;
-        },
-
-        clipRuleTwoPath() {
-            return this.isFilled && this.clipRulePath2 === '' && this.isDoublePath
-                ? this.clipRulePath2
-                : 'evenodd';
-        },
-
-        isDoublePath() {
-            return this.filledIconsWithDoublePath.includes(this.nameFormated);
-        },
-
-        nameFormated() {
-            return this.name.toLowerCase().replace('-', '_');
-        }
+    fillRulePath2: {
+      type: String,
+      default: ''
     },
 
-    mounted() {
-        this.buildIcon();
+    clipRulePath2: {
+      type: String,
+      default: ''
     },
 
-    methods: {
-        buildIcon() {
-            this.buildIconStructure(this.nameFormated);
-        },
+    name: {
+      type: String,
+      required: true
+    },
 
-        buildIconStructure(name) {
-            if(this.isFilled && this.filledIconsWithTriplePaths.includes(name)) {
-                this.generateTriplePathIcon();
+    isFilled: {
+      type: Boolean,
+      default: false
+    },
 
-                return;
-            }
+    fillColor: {
+      type: String,
+      default: 'none'
+    },
 
-            this.generateIcon(name);
-        },
+    fillRule: {
+      type: String,
+      default: 'nonzero'
+    },
 
-        generateIcon(name) {
-            const isDoublePath = this.isFilled
-                ? this.filledIconsWithDoublePath.includes(name)
-                : this.iconsWithDoublePath.includes(name);
+    classIcon: {
+      type: String,
+      default: ''
+    },
 
-            isDoublePath
-                ? this.buildIconDoublePath()
-                : this.icon.path1 = this.getIconPath();
-        },
+    strokeColor: {
+      type: String,
+      default: 'currentColor'
+    },
 
-        getIconPath() {
-            return this.icons[this.nameFormated];
-        },
+    strokeWidth: {
+      type: Number,
+      default: 2
+    },
 
-        buildIconDoublePath() {
-            this.doublePath = true;
+    viewBox: {
+      type: String,
+      default: '0 0 24 24'
+    },
 
-            this.icon = this.getDoublePathIcon();
-        },
+    strokeLinecap: {
+      type: String,
+      default: 'round'
+    },
 
-        generateTriplePathIcon() {
-            this.isTriplePath = true;
+    strokeLinejoin: {
+      type: String,
+      default: 'round'
+    },
 
-            this.icon = this.icosWithTriplePathFilled();
-        },
-
-        icosWithTriplePathFilled() {
-            const {
-                path1,
-                path2,
-            } = this.getDoublePathIcon();
-
-            const path3 = this.icons[this.nameFormated].path3;
-
-            return { path1, path2, path3 };
-        },
-
-        getDoublePathIcon() {
-            const {
-                path1,
-                path2,
-            } = this.icons[this.nameFormated];
-
-            return { path1, path2 };
-        },
+    size: {
+      type: [String, Number],
+      default: '4'
     }
+  },
+
+  data: () => ({
+    doublePath: false,
+    isTriplePath: false,
+    icon: {
+      'path1': '',
+      'path2': ''
+    },
+    iconsWithDoublePath: DoublePathIcons,
+    filledIconsWithDoublePath: FilledDoublePathIcons,
+    filledIconsWithTriplePaths: TriplePathFilledIcons,
+  }),
+
+  computed: {
+    iconNameClass() {
+      return this.name.split('_').join('-');
+    },
+
+    classIconFinal () {
+      return `${this.iconNameClass} ${this.classIcon} w-${this.size} h-${this.size}`;
+    },
+
+    clipRuleData() {
+      return this.name == 'volume_off' ? 'evenodd' : this.clipRule;
+    },
+
+    icons() {
+      return this.isFilled
+        ? icons.filled
+        : icons;
+    },
+
+    strokeStyleComponent() {
+      return this.isFilled && this.strokeWidth === 2
+        ? 0
+        : this.strokeWidth;
+    },
+
+    fillStyleComponent() {
+      return this.isFilled && this.fillColor === 'none'
+        ? 'currentColor'
+        : this.fillColor;
+    },
+
+    viewBoxComponent() {
+      return this.isFilled && this.viewBox === '0 0 24 24'
+        ? '0 0 20 20'
+        : this.viewBox;
+    },
+
+    fillRuleBasicPath() {
+      return this.isFilled && this.fillRule === 'nonzero'
+        ? 'evenodd'
+        : this.fillRule;
+    },
+
+    clipRuleBasicPath() {
+      return this.isFilled && this.clipRuleData === 'nonzero'
+        ? 'evenodd'
+        : this.clipRuleData;
+    },
+
+    fillRuleTwoPath() {
+      return this.isFilled && this.fillRulePath2 === '' && this.isDoublePath
+        ? 'evenodd'
+        : this.fillRulePath2;
+    },
+
+    clipRuleTwoPath() {
+      return this.isFilled && this.clipRulePath2 === '' && this.isDoublePath
+        ? this.clipRulePath2
+        : 'evenodd';
+    },
+
+    isDoublePath() {
+      return this.filledIconsWithDoublePath.includes(this.nameFormated);
+    },
+
+    nameFormated() {
+      return this.name.toLowerCase().replace('-', '_');
+    }
+  },
+
+  mounted() {
+    this.buildIcon();
+  },
+
+  methods: {
+    buildIcon() {
+      this.buildIconStructure(this.nameFormated);
+    },
+
+    buildIconStructure(name) {
+      if(this.isFilled && this.filledIconsWithTriplePaths.includes(name)) {
+        this.generateTriplePathIcon();
+
+        return;
+      }
+
+      this.generateIcon(name);
+    },
+
+    generateIcon(name) {
+      const isDoublePath = this.isFilled
+        ? this.filledIconsWithDoublePath.includes(name)
+        : this.iconsWithDoublePath.includes(name);
+
+      isDoublePath
+        ? this.buildIconDoublePath()
+        : this.icon.path1 = this.getIconPath();
+    },
+
+    getIconPath() {
+      return this.icons[this.nameFormated];
+    },
+
+    buildIconDoublePath() {
+      this.doublePath = true;
+
+      this.icon = this.getDoublePathIcon();
+    },
+
+    generateTriplePathIcon() {
+      this.isTriplePath = true;
+
+      this.icon = this.icosWithTriplePathFilled();
+    },
+
+    icosWithTriplePathFilled() {
+      const {
+        path1,
+        path2,
+      } = this.getDoublePathIcon();
+
+      const path3 = this.icons[this.nameFormated].path3;
+
+      return { path1, path2, path3 };
+    },
+
+    getDoublePathIcon() {
+      const {
+        path1,
+        path2,
+      } = this.icons[this.nameFormated];
+
+      return { path1, path2 };
+    },
+  }
 };
 
 // ".*-.*": REGEX TO SEARCH ANY -
